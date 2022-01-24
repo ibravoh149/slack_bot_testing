c572823d99b433c2afd50447edcf15114cf3ad0b
ibravoh149 <ibravoh149@gmail.com>
2022-01-24 11:03:19 +0100

    verifying token

--- src/MiddleWare/verify_key.ts
+++ src/MiddleWare/verify_key.ts
@@ -1,11 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import crypto from "crypto";
 
 export const verify_key = (req: Request, res: Response, next: NextFunction) => {
-  const headers = req.headers;
-  if (typeof headers == "string") {
-    console.log("string HEAder", JSON.parse(headers));
-  } else {
-    console.log(headers);
+  const timeStamp = req.headers["X-Slack-Request-Timestamp"];
+  const slack_sig = req.headers["X-Slack-Signature"];
+  const secret = process.env.SLACK_SIGNING_SECRET;
+
+  if (Math.abs(new Date().getTime() - Number(timeStamp)) > 60 * 5) {
+    return;
   }
+  const request: any = req;
+  const sig_basestring = "v0:" + timeStamp + ":" + request.rawBody;
+
+  const hmac =
+    "v0=" +
+    crypto.createHmac("sha256", secret).update(sig_basestring).digest("hex");
+
+  console.log("is Valid", hmac === slack_sig);
+  // console.log("is Valid");
+
   return next();
 };
