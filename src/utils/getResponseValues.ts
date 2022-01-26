64f093802493324c2c1fcf47daf87b27dcc5c274
ibravoh149 <ibravoh149@gmail.com>
2022-01-26 09:51:45 +0100

    debugging bug

--- src/utils/getResponseValues.ts
+++ src/utils/getResponseValues.ts
@@ -5,8 +5,8 @@ import {
 } from "../Controller/interfaces";
 
 const responseInformation = {
-  [`${ACTION_TYPES.action_2}`]: {
-    key: ACTION_TYPES.action_2,
+  [`${ACTION_TYPES.action_1}`]: {
+    key: ACTION_TYPES.action_1,
     dropDownValues: [
       { text: { text: "Doing Well", type: "plain_text" }, value: "Doing Well" },
       { text: { text: "Neutral", type: "plain_text" }, value: "Neutral" },
@@ -16,12 +16,12 @@ const responseInformation = {
       },
     ],
     message: "Welcome. How are you doing?",
-    next: ACTION_TYPES.action_end,
+    next: ACTION_TYPES.action_2,
     interactive_type: INTERACTIVE_TYPE.static_select,
   },
 
-  [`${ACTION_TYPES.action_1}`]: {
-    key: ACTION_TYPES.action_1,
+  [`${ACTION_TYPES.action_2}`]: {
+    key: ACTION_TYPES.action_2,
     dropDownValues: [
       { text: { text: "Football", type: "plain_text" }, value: "Football" },
       { text: { text: "Music", type: "plain_text" }, value: "Music" },
@@ -30,7 +30,7 @@ const responseInformation = {
       { text: { text: "Basketball", type: "plain_text" }, value: "Basketball" },
     ],
     message: "What are your favorite hobbies",
-    next: ACTION_TYPES.action_2,
+    next: ACTION_TYPES.action_end,
     interactive_type: INTERACTIVE_TYPE.multi_static_select,
   },
 
